488f8748f12d38bef3a8cba91b2956093ba4063c
Thiago <thiago@gmail.com>
2024-06-19 00:40:11 -0400

    Adcionando lg_bsf/funções staticas e atualizando outras funções

--- Unweighted_Graph/src/main.c
+++ Unweighted_Graph/src/main.c
@@ -4,8 +4,19 @@
 
 int main()
 {
-    L_Graph G = lg_makeGraphFromFile("./graphs/grafo_1.txt");
-    if(G != NULL)
-        lg_outputFile(G, "./graphs/saidaL.txt");
+    L_Graph G = lg_makeGraph(10);
+    lg_insertEdge(G, 1, 2);
+    lg_insertEdge(G, 2, 4);
+    lg_insertEdge(G, 2, 3);
+    lg_insertEdge(G, 3, 5);
+    lg_insertEdge(G, 3, 7);
+    lg_insertEdge(G, 4, 5);
+    lg_insertEdge(G, 6, 7);
+    lg_insertEdge(G, 6, 8);
+    lg_insertEdge(G, 7, 8);
+    lg_insertEdge(G, 8, 9);
+    lg_insertEdge(G, 9, 10);
+    lg_show(G);
+    lg_bsf(G, 4, "./graphs/bfsL.txt");
     lg_destroyGraph(&G);
 }
